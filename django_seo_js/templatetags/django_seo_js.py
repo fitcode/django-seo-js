97c525ed7702fe1bb3109edc22d5f2f28c7ec0d4
bahoo <jon.c.culver@gmail.com>
2016-08-23 14:09:09 -0700

    When it's time to mark it we will mark it safe.

--- django_seo_js/templatetags/django_seo_js.py
+++ django_seo_js/templatetags/django_seo_js.py
@@ -1,7 +1,8 @@
 from django import template
+from django.utils.safestring import mark_safe
 register = template.Library()
 
 
-@register.simple_tag(is_safe=True)
+@register.simple_tag()
 def seo_js_head(*args):
-    return """<meta name="fragment" content="!">"""
+    return mark_safe("""<meta name="fragment" content="!">""")
